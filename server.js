2bc112c4d65cdca10cf306cd38e5daf817b9ec02
thana19 <thana.j@gmail.com>
2021-07-13 00:59:52 +0700

    crud

--- server.js
+++ server.js
@@ -12,12 +12,54 @@ app.use(express.json())
 mongoose.connect('mongodb://usr:secure@mongo/test', { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.get('/', (req, res) => {
-  res.json({ message: 'wrk-mongodb!' })
+  res.json({ message: 'node-mongo-demo!7' })
 })
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
     res.json(products)
+})
+
+app.get('/products/:id', async (req, res) => {
+  const { id } = req.params
+  const product = await Product.findById(id)
+  res.json(product)
+})
+
+app.get('/products/name/:pname', async (req, res) => {
+  const { pname } = req.params
+  const product = await Product.findOne({ name: pname })
+  res.json(product)
+})
+
+app.post('/products', async (req, res) => {
+  const payload = req.body
+  const product = new Product(payload)
+  await product.save()
+  res.status(201).end()
+})
+
+app.put('/products/:id', async (req, res) => {
+  const payload = req.body
+  const { id } = req.params
+
+  try {
+    const product = await Product.findByIdAndUpdate(id, { $set: payload })
+    res.json(product)
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+app.delete('/products/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    await Product.findByIdAndDelete(id)
+    res.status(204).end()
+  } catch (error) {
+    res.status(400).json(error)
+  }
 })
 
 app.listen(port, () => {
